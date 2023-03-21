0f28a9e7811fbffcfb6f0305286149f96814cadb
João Vítor Carvalho <jvitor.200003@gmail.com>
2023-03-21 07:29:45 -0300

    refactor: update Node entity properties and add constructor

--- source/domain/entities/Node.ts
+++ source/domain/entities/Node.ts
@@ -1,7 +1,24 @@
 import Position from "@/domain/entities/Position";
 
+export interface INodeConstructorParams<T> {
+  id: string;
+  position: Position;
+  value: T;
+  connectedNodesIds: string[];
+}
+
 export class Node<T = string> {
   id: string;
   position: Position;
   value: T;
+  connectedNodesIds: string[];
+
+  constructor({
+    id, position, value, connectedNodesIds = []
+  }: INodeConstructorParams<T>){
+    this.id = id;
+    this.position = position;
+    this.value = value;
+    this.connectedNodesIds = connectedNodesIds
+  }
 }
