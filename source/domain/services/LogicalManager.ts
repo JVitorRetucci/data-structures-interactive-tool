e419d48c9a3e59999cb93c22ea5e697d1d8ba700
João Vítor Carvalho <jvitor.200003@gmail.com>
2023-03-23 06:54:51 -0300

    feat: add Logical and Position Managers

---
+++ source/domain/services/LogicalManager.ts
@@ -0,0 +1,79 @@
+import { Node as NodeEntity } from "@/domain/entities/Node";
+import { PositionManager } from "@/domain/services/PositionManager";
+import Position from "@/domain/entities/Position";
+
+type Node = NodeEntity<unknown>; // Node.data type is not important here.
+
+export interface ILogicalManagerConstructorParams {
+  initialNodes?: Node[];
+  positionManager: PositionManager;
+}
+
+export class LogicalManager {
+  private _nodes: Node[];
+  private readonly _positionManager: PositionManager;
+
+  constructor({
+    positionManager,
+    initialNodes = [],
+  }: ILogicalManagerConstructorParams) {
+    this._nodes = initialNodes;
+    this._positionManager = positionManager;
+  }
+
+  public get nodes(): Node[] {
+    return this._nodes;
+  }
+
+  public set nodes(nodes: Node[]) {
+    this._nodes = nodes;
+  }
+
+  setTargetNode(node: Node) {
+    this.nodes = this.nodes.reduce((acc, value, index) => {
+      if (value.id === node.id) {
+        acc[index] = node;
+      }
+      return acc;
+    }, this.nodes);
+  }
+
+  getTargetNode(value: Node | string): Node | undefined {
+    const nodeId = typeof value === "string" ? value : value.id;
+
+    return this.nodes.find((_node) => _node.id === nodeId);
+  }
+
+  updatePositions(): void {
+    const result = this._positionManager.updatePositions(this.nodes);
+    if (result.isLeft()) throw result.value;
+
+    this.nodes = this.nodes.map((_node) => {
+      _node.position = result.value[_node.id];
+      return _node;
+    });
+  }
+
+  updateTargetPosition(node: Node): void {
+    const result = this._positionManager.updateTargetPosition(node);
+    if (result.isLeft()) throw result.value;
+
+    const targetNode = this.nodes.find(
+      (_node) => _node.id in result.value
+    ) as Node;
+    targetNode.position = result.value[targetNode.id];
+    this.setTargetNode(targetNode);
+  }
+
+  // getPositions() {
+  //   // TODO: Implement getPositions() method;
+  //   this._positionManager.getPositions(this.nodes);
+  //   throw new Error("Method not implemented");
+  // }
+
+  // getTargetPosition(node: Node): Position {
+  //   // TODO: Implement getTargetPosition() method;
+  //   this._positionManager.getTargetPosition(node);
+  //   throw new Error("Method not implemented");
+  // }
+}
---
+++ source/domain/services/PositionManager.ts
@@ -0,0 +1,14 @@
+import { TEither } from "@/core/Either";
+import { TApplicationError } from "@/core/Errors";
+import { Node as NodeEntity } from "@/domain/entities/Node";
+import Position from "../entities/Position";
+
+type Node = NodeEntity<unknown>; // Node.data type is not important here.
+type NodeId = string;
+
+export abstract class PositionManager {
+  abstract updatePositions(nodes: Node[]): TEither<TApplicationError, Record<NodeId, Position>>;
+  abstract updateTargetPosition(node: Node): TEither<TApplicationError, Record<NodeId, Position>>;
+  // abstract getPositions(nodes: Node[]): TEither<TApplicationError, Record<NodeId, Position>>;
+  // abstract getTargetPosition(node: Node): TEither<TApplicationError, Record<NodeId, Position>>;
+}
