c2ed2e3fa1391c330e3ba169920198d04fe00300
João Vítor Retucci <jvitor.200003@gmail.com>
2023-05-07 00:48:17 -0300

    chore: update ListNode component

--- source/presentation/components/ListNode.tsx
+++ source/presentation/components/ListNode.tsx
@@ -12,10 +12,16 @@ export const ListNode = ({id, data: {value, nextNodeId, isActive = false }}: Nod
     <>
       <Handle type="target" position={Position.Left} />
       <div className={classNames("flex flex-col justify-center w-fit border-black border rounded bg-white", isActive ? 'ring-4 ring-blue-700' : '')}>
-        <span className="text-center p-1 text-xs border-b border-black">{ id }</span>
+        <span className="text-center p-1 text-xs border-b border-black">Id: { id }</span>
         <div className="flex justify-center w-fit">
-          <p className="p-4 border-r border-black">{ value }</p>
-          <p className="p-4">{ nextNodeId }</p>
+          <p className="py-4 px-2 border-r border-black flex flex-col">
+            Value:
+            <strong>{ value }</strong>
+          </p>
+          <p className="py-4 px-2 flex flex-col">
+            Next:
+            <strong>{ nextNodeId }</strong>
+          </p>
         </div>
       </div>
       <Handle type="source" position={Position.Right} />
