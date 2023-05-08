a7ba6eec59145a835573c9353262295e24295100
João Vítor Retucci <jvitor.200003@gmail.com>
2023-05-08 13:10:00 -0300

    feat: add DialogWrapper

--- source/presentation/components/DialogWrapper.tsx
+++ source/presentation/components/DialogWrapper.tsx
@@ -1,28 +1,32 @@
 import { FC, ReactNode } from "react";
-import { Dialog } from '@headlessui/react';
+import { Dialog } from "@headlessui/react";
 
 interface IDialogProps {
   children: ReactNode;
-  isOpen: boolean
+  title?: string;
+  isOpen: boolean;
   setIsOpen: (isOpen: boolean) => void;
 }
 
 export const DialogWrapper: FC<IDialogProps> = ({
+  title,
   isOpen,
-  setIsOpen, 
-  children
+  setIsOpen,
+  children,
 }): JSX.Element => {
   return (
-    <Dialog className="flex justify-center items-center text-white" open={isOpen} onClose={() => setIsOpen(false)}>
-      <div className="fixed inset-0 z-10 bg-black/60 backdrop-blur-sm" aria-hidden="true" />
+    <Dialog
+      className="flex justify-center items-center text-white"
+      open={isOpen}
+      onClose={() => setIsOpen(false)}
+    >
+      <div
+        className="fixed inset-0 z-10 bg-black/60 backdrop-blur-sm"
+        aria-hidden="true"
+      />
       <Dialog.Panel className="z-10 mx-auto bg-gradient-to-b from-slate-700 to-slate-800 px-6 py-4 fixed rounded-md drop-shadow-md top-1/2 -translate-y-1/2">
-        <Dialog.Title>Deactivate account</Dialog.Title>
-        <Dialog.Description>
-          This will permanently deactivate your account
-          { children }
-        </Dialog.Description>
-        <button onClick={() => setIsOpen(false)}>Deactivate</button>
-        <button onClick={() => setIsOpen(false)}>Cancel</button>
+        {!!title && <Dialog.Title>{title}</Dialog.Title>}
+        {children}
       </Dialog.Panel>
     </Dialog>
   );
