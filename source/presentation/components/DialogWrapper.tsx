472ca09c11db6c7085cbf5457e24ef9d2f300ca7
João Vítor Retucci <jvitor.200003@gmail.com>
2023-05-16 20:39:15 -0300

    chore: update DialogWrapper component

--- source/presentation/components/DialogWrapper.tsx
+++ source/presentation/components/DialogWrapper.tsx
@@ -2,17 +2,25 @@ import { FC, ReactNode } from "react";
 import { Dialog } from "@headlessui/react";
 
 interface IDialogProps {
-  children: ReactNode;
+  children?: ReactNode;
   title?: string;
+  message?: string;
+  applyLabel?: string;
+  cancelLabel?: string;
   isOpen: boolean;
   setIsOpen: (isOpen: boolean) => void;
+  apply: () => void;
 }
 
 export const DialogWrapper: FC<IDialogProps> = ({
-  title,
+  title = "",
+  message = "Are you sure?",
+  applyLabel,
+  cancelLabel,
   isOpen,
-  setIsOpen,
   children,
+  setIsOpen,
+  apply,
 }): JSX.Element => {
   return (
     <Dialog
@@ -25,8 +33,27 @@ export const DialogWrapper: FC<IDialogProps> = ({
         aria-hidden="true"
       />
       <Dialog.Panel className="z-10 mx-auto bg-gradient-to-b from-slate-700 to-slate-800 px-6 py-4 fixed rounded-md drop-shadow-md bottom-4 md:bottom-auto right-4 md:right-auto left-4 md:left-auto md:top-1/2 md:-translate-y-1/2">
-        {!!title && <Dialog.Title>{title}</Dialog.Title>}
-        {children}
+        {!!title && (
+          <Dialog.Title className="w-full text-center text-2xl font-semibold mb-5">
+            {title}
+          </Dialog.Title>
+        )}
+        {children ?? (
+          <>
+            <p className="block w-full text-center mb-4">{message}</p>
+            <div className="flex flex-col md:flex-row justify-center items-center gap-4">
+              <button
+                className="w-full bg-dracula-purple/90 hover:bg-gradient-to-br from-dracula-purple to-dracula-purple/50 p-2 rounded text-white"
+                onClick={apply}
+              >
+                {applyLabel ?? "Apply"}
+              </button>
+              <button className="btn-full" onClick={() => setIsOpen(false)}>
+                {cancelLabel ?? "Cancel"}
+              </button>
+            </div>
+          </>
+        )}
       </Dialog.Panel>
     </Dialog>
   );
