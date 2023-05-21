0324dd513019d0207481fc3607cd31ae6452bbc8
João Vítor Retucci <jvitor.200003@gmail.com>
2023-05-21 20:50:53 -0300

    test: add unit tests for ListManager

--- source/infra/positionManagers/ListManager.spec.ts
+++ source/infra/positionManagers/ListManager.spec.ts
@@ -53,4 +53,47 @@ describe("ListManager", () => {
       [idList[4]]: new Position(840, 40),
     });
   });
+
+  it("should return right when updating target node's position successfully", () => {
+    const manager = new ListManager({ padding: 40 });
+    const idList = Array.from({ length: 5 }, () => generateRandomId());
+    const nodes = idList.map(
+      (id: string, index: number, ids: string[]) =>
+        new Node({
+          id,
+          position: new Position(0, 0),
+          value: undefined,
+          connectedNodesIds: [!ids[index + 1] ? "" : ids[index + 1]],
+        })
+    );
+
+    const response = manager.updateTargetPosition(nodes, idList[2]);
+      
+    expect(response.isRight()).toBeTruthy();
+    expect(response.value).toStrictEqual({
+      // [idList[0]]: new Position(0, 0),
+      // [idList[1]]: new Position(0, 0),
+      [idList[2]]: new Position(200, 40),
+      [idList[3]]: new Position(400, 40),
+      [idList[4]]: new Position(600, 40),
+    });
+  });
+
+  it("should return left when target is not found", () => {
+    const manager = new ListManager({ padding: 40 });
+    const idList = Array.from({ length: 5 }, () => generateRandomId());
+    const nodes = idList.map(
+      (id: string, index: number, ids: string[]) =>
+        new Node({
+          id,
+          position: new Position(0, 0),
+          value: undefined,
+          connectedNodesIds: [!ids[index + 1] ? "" : ids[index + 1]],
+        })
+    );
+
+    const response = manager.updateTargetPosition(nodes, "#lorem_id");
+      
+    expect(response.isLeft()).toBeTruthy();
+  });
 });
